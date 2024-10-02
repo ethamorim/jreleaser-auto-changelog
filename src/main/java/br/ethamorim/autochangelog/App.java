a65c56367f5606d9128e3e74521119228cf90d8a
ethamorim <ethamorim@gmail.com>
2024-10-02 17:04:59 -0300

    build: use maven instead

---
+++ src/main/java/br/ethamorim/autochangelog/App.java
@@ -0,0 +1,13 @@
+package main.java.br.ethamorim.autochangelog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
